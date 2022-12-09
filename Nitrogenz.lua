883beab84fc2089cfa88117b2f84c81b1145e46a
Nitrozena <120191423+Nitrozena@users.noreply.github.com>
2022-12-09 15:35:19 +0100

    Create Nitrogenz.lua

---
+++ Nitrogenz.lua
@@ -0,0 +1,31 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("Nitrogens PlaneCrazy Autobuild", "GrapeTheme")
+
+local Home = Window:NewTab("Home")
+    local HomeSection = Home:NewSection("About")
+    
+    HomeSection:NewButton("Copy Discord link", "wtf didnt you understood?", function()
+    setclipboard("https://discord.gg/GQgST9KS5x")
+    toclipboard("https://discord.gg/GQgST9KS5x")
+end)
+
+local Autobuild = Window:NewTab("Autobuild")
+    local AutobuildSection = Autobuild:NewSection("Autobuild")
+    
+    AutobuildSection:NewButton("Fw 190", "An cool plane", function()
+        loadstring(game:HttpGet('https://pastebin.com/raw/yzHdY5VN'))()
+wait(1)
+loadstring(game:HttpGet('https://pastebin.com/raw/938ju6tj'))()
+wait(1)
+loadstring(game:HttpGet('https://pastebin.com/raw/mpTZ24Mn'))()
+wait(1)
+loadstring(game:HttpGet('https://pastebin.com/raw/bABK5D69'))()
+wait(1)
+local A_1 = Vector3.new(0, 2, 0)
+local Event = game:GetService("ReplicatedStorage").Remotes.MoveAircraft
+Event:InvokeServer(A_1)
+wait(1)
+loadstring(game:HttpGet('https://pastebin.com/raw/SqSUn9L1'))()
+wait(1)
+loadstring(game:HttpGet('https://pastebin.com/raw/j1qezV5X'))()
+end)
